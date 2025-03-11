c71a52f6ffa3f29d0dd5b56bc799159263754cba
applecider <arizah.applecider+github@gmail.com>
2025-03-10 19:13:38 -0600

    First endpoint almost done :)

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-// index.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({optionsSuccessStatus: 200}));
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?",(req, res) => {
+  const userDate = req.params.date;
+  const date = new Date(userDate)
+
+  const unix = date.valueOf();
+  const year = date.getUTCFullYear();
+  const month = date.getMonth();
+  const day = date.getUTCDate();
+  const hours = date.getHours();
+  const seconds = date.getSeconds();
+  const miliseconds = date.getMilliseconds();
+  
+  const utc = new Date(Date.UTC(year, month, day, hours, seconds, miliseconds)).toUTCString();
+
+  res.status(200).json({unix: unix , utc: utc});
+
+
 });
 
 
